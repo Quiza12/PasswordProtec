7d2a60ee732a1cab1ebc4be3aa6e257a09b5dad4
Quiza12 <Quiza12@live.com>
2018-10-27 14:03:56 +1100

    Final commit before official launch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
 import { StrengthsService } from './services/strengths/strengths.service';
 import { PasswordService } from './services/password/password.service';
 
@@ -10,37 +9,9 @@ import { PasswordService } from './services/password/password.service';
 })
 
 export class AppComponent implements OnInit {
-  appLinkDisabled:boolean;
-  aboutLinkDisabled:boolean;
-  answersLinkDisabled:boolean;
-  infoLinkDisabled:boolean;
 
-  constructor(private router: Router) {
-    this.appLinkDisabled = false;
-    this.aboutLinkDisabled = false;
-    this.answersLinkDisabled = false;
-    this.infoLinkDisabled = false;
-  }
+  constructor() {}
 
-  ngOnInit() {
-    this.router.events.subscribe((event: NavigationEnd) => {
-      console.log(event.url);
-      switch(event.url) {
-        case '/': this.appLinkDisabled = true;
-        break;
-        case '/about': this.aboutLinkDisabled = true;
-        break;
-        case '/answers': this.answersLinkDisabled = true;
-        break;
-        case '/info': this.infoLinkDisabled = true;
-        break;
-      }
-      console.log(this.appLinkDisabled);
-      console.log(this.aboutLinkDisabled);
-      console.log(this.answersLinkDisabled);
-      console.log(this.infoLinkDisabled);
-    });
-
-  }
+  ngOnInit() {  }
 
 }
